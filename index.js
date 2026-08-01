5afd9e27e714a9d93922d329d69e9cf4d1a6f4cb
PROTIK-HIVE <youok1040@gmail.com>
2026-08-01 19:00:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const login = require("fca-unofficial");
+const login = require("fca-horizon-remake");
 const fs = require("fs");
 const express = require("express");
 const axios = require("axios");
